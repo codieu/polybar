39b67f43429f5507a7c2767ff91762c23de05eb1
codie <codieuwu@gmail.com>
2025-05-08 11:30:53 +0300

    made toggled ipc gets updated automaticly

--- launch.sh
+++ launch.sh
@@ -12,3 +12,17 @@ polybar --reload left 2>&1 | tee -a /tmp/polybar/left.log & disown
 polybar --reload logo 2>&1 | tee -a /tmp/polybar/logo.log & disown
 polybar --reload right 2>&1 | tee -a /tmp/polybar/right.log & disown
 polybar --reload toggled 2>&1 | tee -a /tmp/polybar/toggled.log & disown
+
+# $! gets the pid of tee so we get the prev pid to target polybar
+toggled_pid=`expr $! - 1`
+
+update_toggled () {
+  while true; do
+    tulip t vpn get --polybar $toggled_pid
+    tulip t dnd get --polybar $toggled_pid
+    tulip t nl get --polybar $toggled_pid
+    sleep 1
+  done
+}
+
+update_toggled & disown
